d308c03a6fbf6a14ea09ac35e5e9447bff1a33d3
Matthias Klumpp <matthias@tenstral.net>
2014-03-29 23:59:18 +0100

    Reuse AsImage from appstream-glib, implementations were already almost identical

---
+++ src/as-image.c
@@ -0,0 +1,263 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2014 Matthias Klumpp <matthias@tenstral.net>
+ * Copyright (C)      2014 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU Lesser General Public License Version 3
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SECTION:as-image
+ * @short_description: Object representing a single image used in a screenshot.
+ *
+ * Screenshot may have multiple versions of an image in different resolutions
+ * or aspect ratios. This object allows access to the location and size of a
+ * single image.
+ *
+ * See also: #AsScreenshot
+ */
+
+#include "config.h"
+
+#include "as-image-private.h"
+#include "as-node-private.h"
+#include "as-utils-private.h"
+
+typedef struct _AsImagePrivate	AsImagePrivate;
+struct _AsImagePrivate
+{
+	AsImageKind		 kind;
+	gchar			*url;
+	guint			 width;
+	guint			 height;
+};
+
+G_DEFINE_TYPE_WITH_PRIVATE (AsImage, as_image, G_TYPE_OBJECT)
+
+#define GET_PRIVATE(o) (as_image_get_instance_private (o))
+
+/**
+ * as_image_finalize:
+ **/
+static void
+as_image_finalize (GObject *object)
+{
+	AsImage *image = AS_IMAGE (object);
+	AsImagePrivate *priv = GET_PRIVATE (image);
+
+	g_free (priv->url);
+
+	G_OBJECT_CLASS (as_image_parent_class)->finalize (object);
+}
+
+/**
+ * as_image_init:
+ **/
+static void
+as_image_init (AsImage *image)
+{
+}
+
+/**
+ * as_image_class_init:
+ **/
+static void
+as_image_class_init (AsImageClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	object_class->finalize = as_image_finalize;
+}
+
+
+/**
+ * as_image_kind_from_string:
+ * @kind: the string.
+ *
+ * Converts the text representation to an enumerated value.
+ *
+ * Returns: (transfer full): a #AsImageKind, or %AS_IMAGE_KIND_UNKNOWN for unknown.
+ *
+ **/
+AsImageKind
+as_image_kind_from_string (const gchar *kind)
+{
+	if (g_strcmp0 (kind, "source") == 0)
+		return AS_IMAGE_KIND_SOURCE;
+	if (g_strcmp0 (kind, "thumbnail") == 0)
+		return AS_IMAGE_KIND_THUMBNAIL;
+	return AS_IMAGE_KIND_UNKNOWN;
+}
+
+/**
+ * as_image_kind_to_string:
+ * @kind: the #AsImageKind.
+ *
+ * Converts the enumerated value to an text representation.
+ *
+ * Returns: string version of @kind
+ *
+ **/
+const gchar *
+as_image_kind_to_string (AsImageKind kind)
+{
+	if (kind == AS_IMAGE_KIND_SOURCE)
+		return "source";
+	if (kind == AS_IMAGE_KIND_THUMBNAIL)
+		return "thumbnail";
+	return NULL;
+}
+
+/**
+ * as_image_get_url:
+ * @image: a #AsImage instance.
+ *
+ * Gets the full qualified URL for the image, usually pointing at some mirror.
+ *
+ * Returns: URL
+ *
+ **/
+const gchar *
+as_image_get_url (AsImage *image)
+{
+	AsImagePrivate *priv = GET_PRIVATE (image);
+	return priv->url;
+}
+
+/**
+ * as_image_get_width:
+ * @image: a #AsImage instance.
+ *
+ * Gets the image width.
+ *
+ * Returns: width in pixels
+ *
+ **/
+guint
+as_image_get_width (AsImage *image)
+{
+	AsImagePrivate *priv = GET_PRIVATE (image);
+	return priv->width;
+}
+
+/**
+ * as_image_get_height:
+ * @image: a #AsImage instance.
+ *
+ * Gets the image height.
+ *
+ * Returns: height in pixels
+ *
+ **/
+guint
+as_image_get_height (AsImage *image)
+{
+	AsImagePrivate *priv = GET_PRIVATE (image);
+	return priv->height;
+}
+
+/**
+ * as_image_get_kind:
+ * @image: a #AsImage instance.
+ *
+ * Gets the image kind.
+ *
+ * Returns: the #AsImageKind
+ *
+ **/
+AsImageKind
+as_image_get_kind (AsImage *image)
+{
+	AsImagePrivate *priv = GET_PRIVATE (image);
+	return priv->kind;
+}
+
+/**
+ * as_image_set_url:
+ * @image: a #AsImage instance.
+ * @url: the URL.
+ * @url_len: the size of @url, or -1 if %NULL-terminated.
+ *
+ * Sets the fully-qualified mirror URL to use for the image.
+ *
+ **/
+void
+as_image_set_url (AsImage *image, const gchar *url, gssize url_len)
+{
+	AsImagePrivate *priv = GET_PRIVATE (image);
+	g_free (priv->url);
+	priv->url = as_strndup (url, url_len);
+}
+
+/**
+ * as_image_set_width:
+ * @image: a #AsImage instance.
+ * @width: the width in pixels.
+ *
+ * Sets the image width.
+ *
+ **/
+void
+as_image_set_width (AsImage *image, guint width)
+{
+	AsImagePrivate *priv = GET_PRIVATE (image);
+	priv->width = width;
+}
+
+/**
+ * as_image_set_height:
+ * @image: a #AsImage instance.
+ * @height: the height in pixels.
+ *
+ * Sets the image height.
+ *
+ **/
+void
+as_image_set_height (AsImage *image, guint height)
+{
+	AsImagePrivate *priv = GET_PRIVATE (image);
+	priv->height = height;
+}
+
+/**
+ * as_image_set_kind:
+ * @image: a #AsImage instance.
+ * @kind: the #AsImageKind, e.g. %AS_IMAGE_KIND_THUMBNAIL.
+ *
+ * Sets the image kind.
+ *
+ **/
+void
+as_image_set_kind (AsImage *image, AsImageKind kind)
+{
+	AsImagePrivate *priv = GET_PRIVATE (image);
+	priv->kind = kind;
+}
+
+/**
+ * as_image_new:
+ *
+ * Creates a new #AsImage.
+ *
+ * Returns: (transfer full): a #AsImage
+ *
+ **/
+AsImage *
+as_image_new (void)
+{
+	AsImage *image;
+	image = g_object_new (AS_TYPE_IMAGE, NULL);
+	return AS_IMAGE (image);
+}
---
+++ src/as-image.h
@@ -0,0 +1,104 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*-
+ *
+ * Copyright (C) 2012-2014 Matthias Klumpp <matthias@tenstral.net>
+ * Copyright (C)      2014 Richard Hughes <richard@hughsie.com>
+ *
+ * Licensed under the GNU Lesser General Public License Version 3
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined (__APPSTREAM_H) && !defined (AS_COMPILATION)
+#error "Only <appstream.h> can be included directly."
+#endif
+
+#ifndef __AS_IMAGE_H
+#define __AS_IMAGE_H
+
+#include <glib-object.h>
+
+#define AS_TYPE_IMAGE		(as_image_get_type())
+#define AS_IMAGE(obj)		(G_TYPE_CHECK_INSTANCE_CAST((obj), AS_TYPE_IMAGE, AsImage))
+#define AS_IMAGE_CLASS(cls)	(G_TYPE_CHECK_CLASS_CAST((cls), AS_TYPE_IMAGE, AsImageClass))
+#define AS_IS_IMAGE(obj)	(G_TYPE_CHECK_INSTANCE_TYPE((obj), AS_TYPE_IMAGE))
+#define AS_IS_IMAGE_CLASS(cls)	(G_TYPE_CHECK_CLASS_TYPE((cls), AS_TYPE_IMAGE))
+#define AS_IMAGE_GET_CLASS(obj)	(G_TYPE_INSTANCE_GET_CLASS((obj), AS_TYPE_IMAGE, AsImageClass))
+
+G_BEGIN_DECLS
+
+typedef struct _AsImage		AsImage;
+typedef struct _AsImageClass	AsImageClass;
+
+struct _AsImage
+{
+	GObject			parent;
+};
+
+struct _AsImageClass
+{
+	GObjectClass		parent_class;
+	/*< private >*/
+	void (*_as_reserved1)	(void);
+	void (*_as_reserved2)	(void);
+	void (*_as_reserved3)	(void);
+	void (*_as_reserved4)	(void);
+	void (*_as_reserved5)	(void);
+	void (*_as_reserved6)	(void);
+	void (*_as_reserved7)	(void);
+	void (*_as_reserved8)	(void);
+};
+
+/**
+ * AsImageKind:
+ * @AS_IMAGE_KIND_UNKNOWN:		Type invalid or not known
+ * @AS_IMAGE_KIND_SOURCE:		The source image at full resolution
+ * @AS_IMAGE_KIND_THUMBNAIL:		A thumbnail at reduced resolution
+ *
+ * The image type.
+ **/
+typedef enum {
+	AS_IMAGE_KIND_UNKNOWN,
+	AS_IMAGE_KIND_SOURCE,
+	AS_IMAGE_KIND_THUMBNAIL,
+	/*< private >*/
+	AS_IMAGE_KIND_LAST
+} AsImageKind;
+
+GType		 as_image_get_type		(void);
+AsImage		*as_image_new			(void);
+
+/* helpers */
+AsImageKind	 as_image_kind_from_string	(const gchar	*kind);
+const gchar	*as_image_kind_to_string	(AsImageKind	 kind);
+
+/* getters */
+const gchar	*as_image_get_url		(AsImage	*image);
+guint		 as_image_get_width		(AsImage	*image);
+guint		 as_image_get_height		(AsImage	*image);
+AsImageKind	 as_image_get_kind		(AsImage	*image);
+
+/* setters */
+void		 as_image_set_url		(AsImage	*image,
+						 const gchar	*url,
+						 gssize		 url_len);
+void		 as_image_set_width		(AsImage	*image,
+						 guint		 width);
+void		 as_image_set_height		(AsImage	*image,
+						 guint		 height);
+void		 as_image_set_kind		(AsImage	*image,
+						 AsImageKind	 kind);
+
+G_END_DECLS
+
+#endif /* __AS_IMAGE_H */
