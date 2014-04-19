29ca244553d2eeee7acbc8c4f814b5a30b507517
Matthias Klumpp <matthias@tenstral.net>
2014-04-19 22:38:13 +0200

    Auto-generate Vala API file, if desired

--- data/vapi/generate_vapi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Small helper script to generate vapi files from GIR
-
-vapigen --metadatadir=. -d . --vapidir=. \
-	/usr/share/gir-1.0/Appstream-0.6.gir \
-	--library=appstream \
-	--pkg glib-2.0 --pkg gio-2.0 --pkg gobject-2.0
