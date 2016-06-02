65f44e537542d1b62ab7602a567fc6ab8cf03642
Matthias Klumpp <matthias@tenstral.net>
2016-06-02 23:04:52 +0200

    travis: Use Docker to run the tests
    LLVM/Clang upstream has closed down their APT repo, and there is no easy
    way to get a more recent version of the LLVM stack on Trusty.
    Since we actually want to build on Xenial anyway, we now run the
    AppStream tests in a Xenial Docker container.

---
+++ tests/ci/build_and_test.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -e
+
+echo "C compiler: $CC"
+echo "C++ compiler: $CXX"
+set -x
+
+#
+# This script is supposed to run inside the AppStream Docker container
+# on the CI system.
+#
+
+# configure AppStream build with all flags enabled
+mkdir build && cd build
+cmake -DMAINTAINER=ON -DDOCUMENTATION=ON -DQT=ON -DAPT_SUPPORT=ON -DVAPI=ON ..
+
+# Build, Test & Install
+make all documentation
+make test ARGS=-V
+make install DESTDIR=./install_root/
+
+# Rebuild everything with Sanitizers enabled
+# FIXME: Doesn't work properly with Clang at time, so we only run this test with GCC.
+make clean && cmake -DMAINTAINER=ON -DDOCUMENTATION=ON -DQT=ON -DAPT_SUPPORT=ON -DVAPI=ON -DSANITIZERS=ON ..
+if [ "$CC" != "clang" ]; then make && make test ARGS=-V; fi
