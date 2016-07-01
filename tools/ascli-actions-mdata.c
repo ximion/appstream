96b7b3e6f21346dfc86c063b11287eb8b8a9fd03
Matthias Klumpp <matthias@tenstral.net>
2016-07-01 21:12:25 +0200

    cli: Create metainfo directory if it is missing and respect DESTDIR
    This resolves #52

--- tools/ascli-actions-mdata.c
+++ tools/ascli-actions-mdata.c
@@ -361,20 +361,29 @@ ascli_put_metainfo (const gchar *fname)
 	g_autofree gchar *tmp = NULL;
 	g_autofree gchar *dest = NULL;
 	g_autoptr(GError) error = NULL;
-	const gchar *metainfo_target = "/usr/share/metainfo";
+	g_autofree gchar *metainfo_target;
+	const gchar *root_dir;
 
 	if (fname == NULL) {
 		ascli_print_stderr (_("You need to specify a metadata file."));
 		return 2;
 	}
 
+	/* determine our root directory */
+	root_dir = g_getenv ("DESTDIR");
+	if (root_dir == NULL)
+		metainfo_target = g_strdup ("/usr/share/metainfo");
+	else
+		metainfo_target = g_build_filename (root_dir, "share", "metainfo", NULL);
+
+	g_mkdir_with_parents (metainfo_target, 0755);
 	if (!as_utils_is_writable (metainfo_target)) {
 		ascli_print_stderr (_("Unable to write to '%s', can not install metainfo file."), metainfo_target);
 		return 1;
 	}
 
 	if ((!g_str_has_suffix (fname, ".metainfo.xml")) && (!g_str_has_suffix (fname, ".appdata.xml"))) {
-		ascli_print_stderr (_("Can not copy '%s': File does not have a '.metainfo.xml' or '.appdata.xml' extension."));
+		ascli_print_stderr (_("Can not copy '%s': File does not have a '.metainfo.xml' or '.appdata.xml' suffix."));
 		return 2;
 	}
 
