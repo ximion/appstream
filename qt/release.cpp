f24815d472b9f2a00bf85e07eec3ad4cf4974f58
Aleix Pol <aleixpol@kde.org>
2016-08-22 20:59:16 +0200

    Implement Appstream::Release for the Qt interface (#65)
    This way we can access release information when using libappstreamqt

---
+++ qt/release.cpp
@@ -0,0 +1,136 @@
+/*
+ * Copyright (C) 2016 Aleix Pol Gonzalez <aleixpol@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#include "appstream.h"
+
+#include "release.h"
+
+#include <QDateTime>
+#include <QDebug>
+#include <QUrl>
+
+using namespace Appstream;
+
+class Appstream::ReleaseData : public QSharedData {
+public:
+    ReleaseData(AsRelease* rel) : m_release(rel)
+    {
+        g_object_ref(m_release);
+    }
+
+    ~ReleaseData()
+    {
+        g_object_unref(m_release);
+    }
+
+    bool operator==(const ReleaseData& rd) const
+    {
+        return rd.m_release == m_release;
+    }
+
+    AsRelease* m_release;
+};
+
+Release::Release(_AsRelease* release)
+    : d(new ReleaseData(release))
+{}
+
+Release::Release(const Release &release) = default;
+
+Release::~Release() = default;
+
+Release& Release::operator=(const Release &release) = default;
+
+bool Release::operator==(const Release &other) const
+{
+    if(this->d == other.d) {
+        return true;
+    }
+    if(this->d && other.d) {
+        return *(this->d) == *other.d;
+    }
+    return false;
+}
+
+QString Release::version() const
+{
+    return QString::fromUtf8(as_release_get_version(d->m_release));
+}
+
+QDateTime Release::timestamp() const
+{
+    return QDateTime::fromTime_t(as_release_get_timestamp(d->m_release));
+}
+
+QString Release::description() const
+{
+    return QString::fromUtf8(as_release_get_description(d->m_release));
+}
+
+QString Release::activeLocale() const
+{
+    return QString::fromUtf8(as_release_get_active_locale(d->m_release));
+}
+
+QList<QUrl> Release::locations() const
+{
+    auto urls = as_release_get_locations(d->m_release);
+    QList<QUrl> ret;
+    ret.reserve(urls->len);
+    for(uint i = 0; i<urls->len; ++i) {
+        auto strval = (const gchar*) g_ptr_array_index (urls, i);
+        ret << QUrl(QString::fromUtf8(strval));
+    }
+    return ret;
+}
+
+Checksum Release::checksum() const
+{
+    {
+        auto cs = as_release_get_checksum(d->m_release, AS_CHECKSUM_KIND_SHA256);
+        if (cs)
+            return Checksum { Checksum::Sha256Checksum, QByteArray(cs) };
+    }
+
+    {
+        auto cs = as_release_get_checksum(d->m_release, AS_CHECKSUM_KIND_SHA1);
+        if (cs)
+            return Checksum { Checksum::Sha1Checksum, QByteArray(cs) };
+    }
+    return Checksum { Checksum::NoneChecksum, "" };
+}
+
+QHash<Release::SizeKind, quint64> Release::sizes() const
+{
+    return {
+        { InstalledSize, as_release_get_size(d->m_release, AS_SIZE_KIND_INSTALLED) },
+        { DownloadSize, as_release_get_size(d->m_release, AS_SIZE_KIND_DOWNLOAD) }
+    };
+}
+
+Release::UrgencyKind Release::urgency() const
+{
+    return Release::UrgencyKind(as_release_get_urgency(d->m_release));
+}
+
+QDebug operator<<(QDebug s, const Appstream::Release& release)
+{
+    s.nospace() << "Appstream::Release(" << release.version() << ": " << release.description() << ")";
+    return s.space();
+}
---
+++ qt/release.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2016 Aleix Pol Gonzalez <aleixpol@kde.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef APPSTREAMQT_RELEASE_H
+#define APPSTREAMQT_RELEASE_H
+
+#include <QSharedDataPointer>
+#include <QString>
+#include <QObject>
+#include <QCryptographicHash>
+#include "appstreamqt_export.h"
+
+struct _AsRelease;
+
+namespace Appstream {
+
+class ReleaseData;
+
+struct Checksum {
+    enum ChecksumKind {
+        NoneChecksum,
+        Sha256Checksum,
+        Sha1Checksum,
+        LastChecksum
+    };
+    const ChecksumKind kind;
+    const QByteArray data;
+};
+
+class APPSTREAMQT_EXPORT Release {
+    Q_GADGET
+    public:
+        Release(_AsRelease* release);
+        Release(const Release& release);
+        ~Release();
+
+        Release& operator=(const Release& release);
+        bool operator==(const Release& r) const;
+
+        enum SizeKind {
+            UnknownSize,
+            DownloadSize,
+            InstalledSize,
+            LastSize
+        };
+        Q_ENUM(SizeKind)
+
+        enum UrgencyKind {
+            UnknownUrgency,
+            LowUrgency,
+            MediumUrgency,
+            HighUrgency,
+            CriticalUrgency,
+            LastUrgency
+        };
+        Q_ENUM(UrgencyKind)
+
+        QString version() const;
+
+        QDateTime timestamp() const;
+
+        QString description() const;
+
+        QString activeLocale() const;
+
+        QList<QUrl> locations() const;
+
+        Checksum checksum() const;
+
+        QHash<SizeKind, quint64> sizes() const;
+
+        UrgencyKind urgency() const;
+
+    private:
+        QSharedDataPointer<ReleaseData> d;
+};
+}
+
+APPSTREAMQT_EXPORT QDebug operator<<(QDebug s, const Appstream::Release& release);
+
+#endif // APPSTREAMQT_RELEASE_H
