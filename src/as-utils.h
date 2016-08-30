eb663674cfe36a018a70c1ad8c8fe9106971d0dc
Matthias Klumpp <matthias@tenstral.net>
2016-08-30 05:50:34 +0200

    trivial: Wrap lines

--- src/as-utils.h
+++ src/as-utils.h
@@ -28,7 +28,8 @@ G_BEGIN_DECLS
 gchar		*as_markup_convert_simple (const gchar *markup,
 					   GError **error);
 
-gboolean	as_utils_locale_is_compatible (const gchar *locale1, const gchar *locale2);
+gboolean	as_utils_locale_is_compatible (const gchar *locale1,
+					       const gchar *locale2);
 gboolean	as_utils_is_category_name (const gchar *category_name);
 gboolean	as_utils_is_tld (const gchar *tld);
 gboolean	as_utils_is_desktop_environment (const gchar *desktop);
