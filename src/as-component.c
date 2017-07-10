ad4b83e90ddc7ca326ab930dfb2d978449e3d774
Matthias Klumpp <matthias@tenstral.net>
2017-07-10 20:29:04 +0200

    trivial: Style
    Also, actually something that looks like a very unnecessary statement,
    but better be sure here...

--- src/as-component.c
+++ src/as-component.c
@@ -379,7 +379,7 @@ as_component_init (AsComponent *cpt)
 
 	priv->token_cache = g_hash_table_new_full (g_str_hash, g_str_equal, g_free, g_free);
 
-	as_component_set_priority (cpt, 0);
+	priv->priority = 0;
 }
 
 /**
