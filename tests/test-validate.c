86a942255ea9f250a207ade94b6f7a70228f84d9
Matthias Klumpp <matthias@tenstral.net>
2017-10-21 23:21:45 +0200

    tests: Add unit to test the data validation code as well

---
+++ tests/test-validate.c
@@ -0,0 +1,52 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2014-2017 Matthias Klumpp <matthias@tenstral.net>
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <glib.h>
+#include "appstream.h"
+#include "as-component-private.h"
+
+#include "as-test-utils.h"
+
+static gchar *datadir = NULL;
+
+int
+main (int argc, char **argv)
+{
+	int ret;
+
+	if (argc == 0) {
+		g_error ("No test directory specified!");
+		return 1;
+	}
+
+	g_assert (argv[1] != NULL);
+	datadir = g_build_filename (argv[1], "samples", NULL);
+	g_assert (g_file_test (datadir, G_FILE_TEST_EXISTS) != FALSE);
+
+	g_setenv ("G_MESSAGES_DEBUG", "all", TRUE);
+	g_test_init (&argc, &argv, NULL);
+
+	/* only critical and error are fatal */
+	g_log_set_fatal_mask (NULL, G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL);
+
+	ret = g_test_run ();
+	g_free (datadir);
+	return ret;
+}
