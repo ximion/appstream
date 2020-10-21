8bce76b8b85c91b567f2ddd44008b0c026441406
Matthias Klumpp <matthias@tenstral.net>
2020-10-21 22:25:27 +0200

    compose: Add boilerplate

---
+++ compose/appstream-compose.h
@@ -0,0 +1,39 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2012-2020 Matthias Klumpp <matthias@tenstral.net>
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __APPSTREAM_COMPOSE_H
+#define __APPSTREAM_COMPOSE_H
+
+#define __APPSTREAM_COMPOSE_H_INSIDE__
+
+#include <appstream.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO */
+
+#ifdef __cplusplus
+};
+#endif
+#undef __APPSTREAM_COMPOSE_H_INSIDE__
+
+#endif /* __APPSTREAM_COMPOSE_H */
