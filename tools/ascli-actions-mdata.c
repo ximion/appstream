d85e0a7893ae857204e5ec3f0f4891cfb2e8ac4a
Philip Withnall <pwithnall@endlessos.org>
2021-02-10 15:22:31 +0000

    ascli: Don’t use GNU C extension type names
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>

--- tools/ascli-actions-mdata.c
+++ tools/ascli-actions-mdata.c
@@ -209,7 +209,7 @@ ascli_what_provides (const gchar *cachepath, const gchar *kind_str, const gchar 
 
 	kind = as_provided_kind_from_string (kind_str);
 	if (kind == AS_PROVIDED_KIND_UNKNOWN) {
-		uint i;
+		unsigned int i;
 		g_printerr ("%s\n", _("Invalid type for provided item selected. Valid values are:"));
 		for (i = 1; i < AS_PROVIDED_KIND_LAST; i++)
 			g_printerr (" • %s\n", as_provided_kind_to_string (i));
