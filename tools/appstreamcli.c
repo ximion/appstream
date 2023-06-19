ximion/appstream

commit 3f88c4e769bb00986e38875736913012f23f903d

cli: Add new is-satisfied check to test relations from the command-line

tools/appstreamcli.c
@@ -543,6 +543,33 @@ as_client_run_check_license (const gchar *command, char **argv, int argc)
 }
 
 /**
+ * as_client_run_is_satisfied:
+ *
+ * Test if a component has its relations satisfied on the current system.
+ */
+static int
+as_client_run_is_satisfied (const gchar *command, char **argv, int argc)
+{
+	g_autoptr(GOptionContext) opt_context = NULL;
+	gint ret;
+	const gchar *fname_or_cid = NULL;
+
+	opt_context = as_client_new_subcommand_option_context (command, find_options);
+	g_option_context_add_main_entries (opt_context, data_collection_options, NULL);
+
+	ret = as_client_option_context_parse (opt_context, command, &argc, &argv);
+	if (ret != 0)
+		return ret;
+
+	if (argc > 2)
+		fname_or_cid = argv[2];
+
+	return ascli_check_is_satisfied (fname_or_cid,
+					 optn_cachepath,
+					 optn_no_cache);
+}
+
+/**
  * as_client_run_put:
  *
  * Place a metadata file in the right directory.
@@ -1315,6 +1342,11 @@ as_client_run (char **argv, int argc)
 			/* TRANSLATORS: `appstreamcli `check-license` command description. */
 			_("Check license string for validity and print details about it."),
 			as_client_run_check_license);
+	ascli_add_cmd (commands,
+			2, "is-satisfied", NULL, "FILE|COMPONENT-ID",
+			/* TRANSLATORS: `appstreamcli `check-license` command description. */
+			_("Check if requirements of a component (via its ID or MetaInfo file) are satisfied on this system."),
+			as_client_run_is_satisfied);
 
 	ascli_add_cmd (commands,
 			3, "install", NULL, "COMPONENT-ID",

src/as-relation.c
@@ -1602,6 +1602,35 @@ _as_get_control_missing_message (AsControlKind c_kind, AsRelationKind r_kind)
 }
 
 /**
+ * _as_get_control_found_message:
+ */
+static gchar*
+_as_get_control_found_message (AsControlKind c_kind)
+{
+	if (c_kind == AS_CONTROL_KIND_POINTING)
+		return g_strdup (_("Pointing device (e.g. a mouse or touchpad) found."));
+	if (c_kind == AS_CONTROL_KIND_KEYBOARD)
+		return g_strdup (_("Physical keyboard found."));
+	if (c_kind == AS_CONTROL_KIND_GAMEPAD)
+		return g_strdup (_("Gamepad found."));
+	if (c_kind == AS_CONTROL_KIND_TV_REMOTE)
+		return g_strdup (_("TV remote found."));
+	if (c_kind == AS_CONTROL_KIND_TABLET)
+		return g_strdup (_("Graphics tablet found."));
+
+	if (c_kind == AS_CONTROL_KIND_TOUCH)
+		return g_strdup (_("Touch input device found."));
+	if (c_kind == AS_CONTROL_KIND_VOICE)
+		return g_strdup (_("Microphone for voice input control found."));
+	if (c_kind == AS_CONTROL_KIND_VISION)
+		return g_strdup (_("Camera for input control found."));
+	if (c_kind == AS_CONTROL_KIND_CONSOLE)
+		return g_strdup (_("Console interface available."));
+
+	return NULL;
+}
+
+/**
  * as_relation_is_satisfied:
  * @relation: a #AsRelation instance.
  * @system_info: (nullable): an #AsSystemInfo to use for system information.
@@ -1664,6 +1693,10 @@ as_relation_is_satisfied (AsRelation *relation,
 				_as_set_satify_message (message,
 							g_strdup_printf (_("Recommended software component '%s' is missing."),
 									cid));
+			else if (priv->kind == AS_RELATION_KIND_SUPPORTS)
+				_as_set_satify_message (message,
+							g_strdup_printf (_("Found supported software component '%s'."),
+									cid));
 			return AS_CHECK_RESULT_FALSE;
 		}
 	}
@@ -1713,9 +1746,9 @@ as_relation_is_satisfied (AsRelation *relation,
 
 	/* Kernels */
 	if (priv->item_kind == AS_RELATION_ITEM_KIND_KERNEL) {
-		const gchar *req_kernel_name;
 		const gchar *current_kernel_name;
 		const gchar *current_kernel_version;
+		const gchar *req_kernel_name;
 		const gchar *req_kernel_version;
 
 		current_kernel_name = as_system_info_get_kernel_name (sysinfo);
@@ -1765,19 +1798,37 @@ as_relation_is_satisfied (AsRelation *relation,
 					   AS_VERCMP_FLAG_NONE)) {
 			const gchar *compare_symbols = as_relation_compare_to_symbols_string (as_relation_get_compare (relation));
 			if (priv->kind == AS_RELATION_KIND_REQUIRES)
-				/* TRANSLATORS: We checked a kernel dependency, the first placeholder is the kernel name,
-				 * second is comparison operator (e.g. >=), third is the expected version number and fourth is the version we are running. */
+				/* TRANSLATORS: We checked a kernel dependency, the first placeholder is the required kernel name,
+				 * second is comparison operator (e.g. >=), third is the expected version number fourth the current kernel name
+				 * and fifth is the version we are running. */
 				_as_set_satify_message (message,
-							g_strdup_printf (_("This software requires %s %s %s, but this system is running version %s."),
-									current_kernel_name, compare_symbols, req_kernel_version, current_kernel_version));
+							g_strdup_printf (_("This software requires %s %s %s, but this system is running %s %s."),
+									req_kernel_name, compare_symbols, req_kernel_version,
+									current_kernel_name, current_kernel_version));
 			else if (priv->kind == AS_RELATION_KIND_RECOMMENDS)
-				/* TRANSLATORS: We checked a kernel dependency, the first placeholder is the kernel name,
-				 * second is comparison operator (e.g. >=), third is the expected version number and fourth is the version we are running. */
+				/* TRANSLATORS: We checked a kernel dependency, the first placeholder is the required kernel name,
+				 * second is comparison operator (e.g. >=), third is the expected version number fourth the current kernel name
+				 * and fifth is the version we are running. */
 				_as_set_satify_message (message,
-							g_strdup_printf (_("The use of %s %s %s is recommended, but this system is running version %s."),
-									current_kernel_name, compare_symbols, req_kernel_version, current_kernel_version));
+							g_strdup_printf (_("The use of %s %s %s is recommended, but this system is running %s %s."),
+									req_kernel_name, compare_symbols, req_kernel_version,
+									current_kernel_name, current_kernel_version));
+			else if (priv->kind == AS_RELATION_KIND_SUPPORTS) {
+				/* TRANSLATORS: We checked a kernel dependency, the first placeholder is the kernel name,
+				 * second is comparison operator (e.g. >=), third is the expected version number. */
+				_as_set_satify_message (message,
+							g_strdup_printf (_("This software supports %s %s %s."),
+									req_kernel_name, compare_symbols, req_kernel_version));
+				/* this is not an error, supports is only a hint for kernels */
+				return AS_CHECK_RESULT_TRUE;
+			}
 			return AS_CHECK_RESULT_FALSE;
 		}
+
+		/* TRANSLATORS: We checked a kernel dependency, with success, the first placeholder is the current kernel name, second is its version number. */
+		_as_set_satify_message (message,
+					g_strdup_printf (_("Kernel %s %s is supported."),
+							 current_kernel_name, current_kernel_version));
 
 		/* if we are here, we are running an acceptable kernel version */
 		return AS_CHECK_RESULT_TRUE;
@@ -1813,8 +1864,20 @@ as_relation_is_satisfied (AsRelation *relation,
 				_as_set_satify_message (message,
 							g_strdup_printf (_("This software recommends %s %.2f GiB of memory, but this system has %.2f GiB."),
 									compare_symbols, req_memory / 1024.0, current_memory / 1024.0));
+			else if (priv->kind == AS_RELATION_KIND_SUPPORTS) {
+				/* TRANSLATORS: We checked a memory dependency, the first placeholder is the comparison operator (e.g. >=),
+				 * second is the expected amount of memory. */
+				_as_set_satify_message (message,
+							g_strdup_printf (_("This software supports %s %.2f GiB of memory."),
+									compare_symbols, req_memory / 1024.0));
+				/* this is not an error, supports is only a hint for memory */
+				return AS_CHECK_RESULT_TRUE;
+			}
 			return AS_CHECK_RESULT_FALSE;
 		}
+
+		_as_set_satify_message (message,
+					g_strdup_printf (_("This system has sufficient memory for this software.")));
 
 		/* if we are here, we have sufficient memory */
 		return AS_CHECK_RESULT_TRUE;
@@ -1838,6 +1901,10 @@ as_relation_is_satisfied (AsRelation *relation,
 		if (res == AS_CHECK_RESULT_FALSE || priv->kind == AS_RELATION_KIND_SUPPORTS)
 			_as_set_satify_message (message,
 						_as_get_control_missing_message (control_kind, priv->kind));
+		if (res == AS_CHECK_RESULT_TRUE)
+			_as_set_satify_message (message,
+						_as_get_control_found_message (control_kind));
+
 		return res;
 	}
 
@@ -1909,6 +1976,9 @@ as_relation_is_satisfied (AsRelation *relation,
 			}
 			return AS_CHECK_RESULT_FALSE;
 		}
+
+		_as_set_satify_message (message,
+					g_strdup (_("Display size is sufficient for this software.")));
 
 		/* if we are here, we have sufficient memory */
 		return AS_CHECK_RESULT_TRUE;

tools/ascli-actions-mdata.c
@@ -581,3 +581,157 @@ ascli_create_metainfo_template (const gchar *out_fname, const gchar *cpt_kind_st
 
 	return 0;
 }
+
+/**
+ * ascli_print_satisfy_check_results:
+ *
+ * Helper for %ascli_check_is_satisfied
+ */
+static gboolean
+ascli_print_satisfy_check_results (GPtrArray *relations, AsSystemInfo *sysinfo, AsPool *pool, AsRelationKind kind)
+{
+	gboolean res = TRUE;
+	const gchar *fail_char = ASCLI_CHAR_FAIL;
+
+	if (kind == AS_RELATION_KIND_SUPPORTS)
+		fail_char = "•";
+
+	for (guint i = 0; i < relations->len; i++) {
+		AsRelation *relation = AS_RELATION (g_ptr_array_index (relations, i));
+		g_autofree gchar *message = NULL;
+		g_autoptr(GError) tmp_error = NULL;
+		AsCheckResult r;
+
+		r = as_relation_is_satisfied (relation,
+						sysinfo,
+						pool,
+						&message,
+						&tmp_error);
+		if (r == AS_CHECK_RESULT_ERROR) {
+			if (as_relation_get_item_kind (relation) == AS_RELATION_ITEM_KIND_DISPLAY_LENGTH &&
+				as_system_info_get_display_length (sysinfo, AS_DISPLAY_SIDE_KIND_LONGEST) == 0) {
+				g_print ("  • %s\n", _("Unable to check display size: Can not read information without GUI toolkit access."));
+			} else {
+				g_print ("  %s %s: %s\n", fail_char, _("ERROR"), tmp_error->message);
+				res = FALSE;
+			}
+		} else if (r == AS_CHECK_RESULT_TRUE) {
+			g_print ("  %s %s\n", ASCLI_CHAR_SUCCESS, message);
+		} else {
+			g_print ("  %s %s\n", fail_char, message);
+			res = FALSE;
+		}
+	}
+
+	return res;
+}
+
+/**
+ * ascli_check_is_satisfied:
+ *
+ * Verify if the selected component is satisfied on the current system.
+ */
+gint
+ascli_check_is_satisfied (const gchar *fname_or_cid, const gchar *cachepath, gboolean no_cache)
+{
+	g_autoptr(AsComponent) cpt = NULL;
+	g_autoptr(GError) error = NULL;
+	g_autoptr(AsPool) pool = NULL;
+	g_autoptr(AsSystemInfo) sysinfo = NULL;
+	GPtrArray *requires, *recommends, *supports;
+	gboolean res = TRUE;
+
+	if (fname_or_cid == NULL) {
+		ascli_print_stderr (_("You need to specify a MetaInfo filename or component ID."));
+		return 2;
+	}
+
+	/* open the metadata pool with default options */
+	pool = ascli_data_pool_new_and_open (cachepath, no_cache, &error);
+	if (error != NULL) {
+		g_printerr ("%s\n", error->message);
+		return ASCLI_EXIT_CODE_FAILED;
+	}
+
+	/* get the current component, either from file or from the pool */
+	if (g_strstr_len (fname_or_cid, -1, "/") != NULL || g_file_test (fname_or_cid, G_FILE_TEST_EXISTS)) {
+		g_autoptr(GFile) mi_file = NULL;
+		g_autoptr(AsMetadata) mdata = NULL;
+
+		mi_file = g_file_new_for_path (fname_or_cid);
+		if (!g_file_query_exists (mi_file, NULL)) {
+			ascli_print_stderr (_("Metainfo file '%s' does not exist."), fname_or_cid);
+			return 4;
+		}
+
+		/* read the metainfo file */
+		mdata = as_metadata_new ();
+		as_metadata_set_locale (mdata, "ALL");
+
+		as_metadata_parse_file (mdata,
+					mi_file,
+					AS_FORMAT_KIND_XML,
+					&error);
+		if (error != NULL) {
+			g_printerr ("%s\n", error->message);
+			return 1;
+		}
+
+		cpt = g_object_ref (as_metadata_get_component (mdata));
+	} else {
+		g_autoptr(GPtrArray) cpts = NULL;
+
+		cpts = as_pool_get_components_by_id (pool, fname_or_cid);
+		if (cpts->len == 0) {
+			ascli_print_stderr (_("Unable to find component with ID '%s'!"), fname_or_cid);
+			return ASCLI_EXIT_CODE_NO_RESULT;
+		}
+
+		cpt = g_object_ref (AS_COMPONENT (g_ptr_array_index (cpts, 0)));
+	}
+
+	requires = as_component_get_requires (cpt);
+	recommends = as_component_get_recommends (cpt);
+	supports = as_component_get_supports (cpt);
+
+	sysinfo = as_system_info_new ();
+
+	/* TRANSLATORS: We are testing the relations (requires, recommends & supports data) for being satisfied on the current system. */
+	ascli_print_stdout (_("Relation check for: %s"), as_component_get_data_id (cpt));
+	g_print ("\n");
+
+	ascli_print_highlight (_("Requirements:"));
+	if (requires->len == 0) {
+		g_print ("  • %s\n", _("No required items are set for this software."));
+	} else {
+		res = ascli_print_satisfy_check_results (requires,
+							 sysinfo,
+							 pool,
+							 AS_RELATION_KIND_REQUIRES)? res : FALSE;
+	}
+
+	ascli_print_highlight (_("Recommendations:"));
+	if (recommends->len == 0) {
+		g_print ("  • %s\n", _("No recommended items are set for this software."));
+	} else {
+		res = ascli_print_satisfy_check_results (recommends,
+							 sysinfo,
+							 pool,
+							 AS_RELATION_KIND_RECOMMENDS)? res : FALSE;
+	}
+
+	ascli_print_highlight (_("Supported:"));
+	if (supports->len == 0) {
+		g_print ("  • %s\n", _("No supported items are set for this software."));
+	} else {
+		ascli_print_satisfy_check_results (supports,
+						   sysinfo,
+						   pool,
+						   AS_RELATION_KIND_SUPPORTS);
+	}
+
+
+
+
+	return res? ASCLI_EXIT_CODE_SUCCESS : ASCLI_EXIT_CODE_FAILED;
+}

tools/ascli-actions-mdata.h
@@ -68,6 +68,10 @@ int		ascli_create_metainfo_template (const gchar *out_fname,
 						const gchar *cpt_kind_str,
 						const gchar *desktop_file);
 
+gint		ascli_check_is_satisfied (const gchar *fname_or_cid,
+					  const gchar *cachepath,
+					  gboolean no_cache);
+
 
 G_END_DECLS
 

commit 3ac199f10736585e7196132474abe7e2cebc2d79

cli: Fix copy/paste error in translators comment
Fixes: 3f88c4e769bb ("Add new is-satisfied check to test relations from the command-line")

tools/appstreamcli.c
@@ -1340,7 +1340,7 @@ as_client_run (char **argv, int argc)
 			as_client_run_check_license);
 	ascli_add_cmd (commands,
 			2, "is-satisfied", NULL, "FILE|COMPONENT-ID",
-			/* TRANSLATORS: `appstreamcli `check-license` command description. */
+			/* TRANSLATORS: `appstreamcli `is-satisfied` command description. */
 			_("Check if requirements of a component (via its ID or MetaInfo file) are satisfied on this system."),
 			as_client_run_is_satisfied);
 