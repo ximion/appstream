ximion/appstream

commit 808326a35895ea7b343eef3b5a9e67928c4af99d

win32: change some file/path-related code for portability
- replace fnmatch with g_pattern_match_simple
- use GDir
- use GFile wherever appropriate

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

src/as-distro-extras.c
@@ -62,18 +62,18 @@ static gboolean
 directory_is_empty (const gchar *dirname)
 {
 	gint n = 0;
-	struct dirent *d;
-	DIR *dir = opendir (dirname);
+	const gchar *d;
+	GDir *dir = g_dir_open (dirname, 0, NULL);
 
 	if (dir == NULL)
 		return TRUE;
 
-	while ((d = readdir (dir)) != NULL) {
+	while ((d = g_dir_read_name (dir)) != NULL) {
 		if (++n > 2)
 			break;
 	}
 
-	closedir (dir);
+	g_dir_close (dir);
 
 	/* empty directory contains . and .. */
 	if (n <= 2)
@@ -223,6 +223,7 @@ as_pool_scan_apt (AsPool *pool, gboolean force, GError **error)
 {
 	g_autoptr(GPtrArray) yml_files = NULL;
 	g_autoptr(GError) tmp_error = NULL;
+	g_autoptr(GFile) file_catalog = NULL;
 	gboolean data_changed = FALSE;
 	gboolean icons_available = FALSE;
 	gboolean yaml_target_dir_exists = FALSE;
@@ -350,7 +351,8 @@ as_pool_scan_apt (AsPool *pool, gboolean force, GError **error)
 
 		/* create compatibility symlink for old location */
 		if (!g_file_test (appstream_catalog_legacy_root, G_FILE_TEST_EXISTS)) {
-			if (symlink (appstream_catalog_root, appstream_catalog_legacy_root) != 0)
+			file_catalog = g_file_new_for_path (appstream_catalog_root);
+			if (!g_file_make_symbolic_link (file_catalog, appstream_catalog_legacy_root, NULL, NULL))
 				g_debug ("Unable to create compatibility symlink '%s': %s",
 					 appstream_catalog_legacy_root, g_strerror (errno));
 		}
@@ -361,6 +363,7 @@ as_pool_scan_apt (AsPool *pool, gboolean force, GError **error)
 		g_autofree gchar *dest_fname = NULL;
 		g_autofree gchar *origin = NULL;
 		g_autofree gchar *file_baseprefix = NULL;
+		g_autoptr(GFile) file_fname = NULL;
 		guint j;
 		const gchar *fname = (const gchar*) g_ptr_array_index (yml_files, i);
 
@@ -373,7 +376,8 @@ as_pool_scan_apt (AsPool *pool, gboolean force, GError **error)
 			continue;
 		} else if (!g_file_test (dest_fname, G_FILE_TEST_EXISTS)) {
 			/* file not found, let's symlink */
-			if (symlink (fname, dest_fname) != 0) {
+			file_fname = g_file_new_for_path (fname);
+			if (!g_file_make_symbolic_link (file_fname, dest_fname, NULL, NULL)) {
 				g_debug ("Unable to set symlink (%s -> %s): %s",
 							fname,
 							dest_fname,

compose/asc-utils-l10n.c
@@ -29,7 +29,7 @@
 #include "config.h"
 #include "asc-utils-l10n.h"
 
-#include <fnmatch.h>
+#include <glib.h>
 
 #include "as-utils-private.h"
 #include "asc-globals.h"
@@ -177,7 +177,7 @@ asc_l10n_search_translations_gettext (AscLocaleContext *ctx,
 		for (guint j = 0; j < contents->len; j++) {
 			const gchar *fname = g_ptr_array_index (contents, j);
 			g_auto(GStrv) segments = NULL;
-			if (fnmatch (match_path, fname, FNM_NOESCAPE) != 0)
+			if (!g_pattern_match_simple (match_path, fname))
 				continue;
 
 			/* fetch locale name from path */
@@ -363,7 +363,7 @@ asc_l10n_search_translations_qt (AscLocaleContext *ctx,
 				const gchar *fname = g_ptr_array_index (contents, j);
 				if (!g_str_has_suffix (fname, ".qm"))
 					continue;
-				if (fnmatch (match_path, fname, FNM_NOESCAPE) != 0)
+				if (!g_pattern_match_simple (match_path, fname))
 					continue;
 
 				segments = g_strsplit (fname + prefix_len, G_DIR_SEPARATOR_S, 4);

src/as-component.c
@@ -23,7 +23,6 @@
 
 #include <glib.h>
 #include <glib-object.h>
-#include <fnmatch.h>
 
 #include "as-utils.h"
 #include "as-utils-private.h"
@@ -3949,7 +3948,7 @@ as_component_is_free (AsComponent *cpt)
 
 		/* return a free component if any of the origin wildcards matches */
 		for (guint i = 0; origin_globs[i] != NULL; i++) {
-			if (fnmatch (origin_globs[i], priv->origin, FNM_NOESCAPE) == 0)
+			if (g_pattern_match_simple (origin_globs[i], priv->origin))
 				return TRUE;
 		}
 	}

src/as-provided.c
@@ -24,7 +24,6 @@
 #include <config.h>
 #include <glib/gi18n-lib.h>
 #include <glib.h>
-#include <fnmatch.h>
 
 #include "as-utils.h"
 
@@ -255,7 +254,7 @@ as_provided_has_item (AsProvided *prov, const gchar *item)
 
 		/* modalias entries may provide wildcards, we match them by default */
 		if (priv->kind == AS_PROVIDED_KIND_MODALIAS) {
-			if (fnmatch (pitem, item, FNM_NOESCAPE) == 0)
+			if (g_pattern_match_simple (pitem, item))
 				return TRUE;
 		}
 	}

src/as-system-info.c
@@ -41,7 +41,7 @@
 #include <errno.h>
 #include <sys/utsname.h>
 #include <dirent.h>
-#include <fnmatch.h>
+#include <glib.h>
 
 #if defined(__linux__)
 #include <sys/sysinfo.h>
@@ -569,7 +569,7 @@ as_system_info_has_device_matching_modalias (AsSystemInfo *sysinfo, const gchar 
 		if (g_strcmp0 (modalias, modalias_glob) == 0)
 			return TRUE;
 
-		if (fnmatch (modalias, modalias_glob, FNM_NOESCAPE) == 0)
+		if (g_pattern_match_simple (modalias, modalias_glob))
 			return TRUE;
 	}
 

commit f59621d1673284a450b336f6f0a1323a12dabe09

apt: Fix wrong symlink order
This was broken in 808326a35895ea7b343eef3b5a9e67928c4af99d

Resolves: #513

src/as-distro-extras.c
@@ -230,7 +230,6 @@ as_pool_scan_apt (AsPool *pool, gboolean force, GError **error)
 {
 	g_autoptr(GPtrArray) yml_files = NULL;
 	g_autoptr(GError) tmp_error = NULL;
-	g_autoptr(GFile) file_catalog = NULL;
 	gboolean data_changed = FALSE;
 	gboolean icons_available = FALSE;
 	gboolean yaml_target_dir_exists = FALSE;
@@ -375,9 +374,12 @@ as_pool_scan_apt (AsPool *pool, gboolean force, GError **error)
 
 		/* create compatibility symlink for old location */
 		if (!g_file_test (appstream_catalog_legacy_root, G_FILE_TEST_EXISTS)) {
-			file_catalog = g_file_new_for_path (appstream_catalog_root);
-			if (!g_file_make_symbolic_link (file_catalog,
-							appstream_catalog_legacy_root,
+			g_autoptr(GFile) symfile_legacy_catalog = NULL;
+
+			symfile_legacy_catalog = g_file_new_for_path (
+			    appstream_catalog_legacy_root);
+			if (!g_file_make_symbolic_link (symfile_legacy_catalog,
+							appstream_catalog_root,
 							NULL,
 							NULL))
 				g_debug ("Unable to create compatibility symlink '%s': %s",
@@ -391,8 +393,6 @@ as_pool_scan_apt (AsPool *pool, gboolean force, GError **error)
 		g_autofree gchar *dest_fname = NULL;
 		g_autofree gchar *origin = NULL;
 		g_autofree gchar *file_baseprefix = NULL;
-		g_autoptr(GFile) file_fname = NULL;
-		guint j;
 		const gchar *fname = (const gchar *) g_ptr_array_index (yml_files, i);
 
 		fbasename = g_path_get_basename (fname);
@@ -403,12 +403,14 @@ as_pool_scan_apt (AsPool *pool, gboolean force, GError **error)
 			g_debug ("File %s is a broken symlink, skipping.", fname);
 			continue;
 		} else if (!g_file_test (dest_fname, G_FILE_TEST_EXISTS)) {
+			g_autoptr(GFile) sym_file = NULL;
+
 			/* file not found, let's symlink */
-			file_fname = g_file_new_for_path (fname);
-			if (!g_file_make_symbolic_link (file_fname, dest_fname, NULL, NULL)) {
+			sym_file = g_file_new_for_path (dest_fname);
+			if (!g_file_make_symbolic_link (sym_file, fname, NULL, NULL)) {
 				g_debug ("Unable to set symlink (%s -> %s): %s",
-					 fname,
 					 dest_fname,
+					 fname,
 					 g_strerror (errno));
 				continue;
 			}
@@ -434,7 +436,7 @@ as_pool_scan_apt (AsPool *pool, gboolean force, GError **error)
 						 strlen (g_strrstr (fbasename, "_") + 1));
 
 		/* extract icons to their destination (if they exist at all */
-		for (j = 0; default_icon_sizes[j] != NULL; j++) {
+		for (guint j = 0; default_icon_sizes[j] != NULL; j++) {
 			as_extract_icon_cache_tarball (appstream_icons_target,
 						       origin,
 						       file_baseprefix,