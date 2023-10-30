2df8bc13eb49f9df33965c3bc7d22a6095701560
Matthias Klumpp <matthias@tenstral.net>
2023-10-30 23:14:44 +0100

    trivial: Remove Cirrus config in release tarballs

--- release.sh
+++ release.sh
@@ -74,6 +74,7 @@ find ./release-tar-tmp -name '*~' -type f -delete
 find ./release-tar-tmp -name '*.bak' -type f -delete
 find ./release-tar-tmp -name '*.o' -type f -delete
 rm -r $R_ROOT/.github
+rm -f $R_ROOT/.cirrus-ci.yml
 rm $R_ROOT/release.sh
 
 # create release tarball
