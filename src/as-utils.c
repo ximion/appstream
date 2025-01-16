92d33c3a44a6b20195f1774c3c8d8f91d993354f
Milan Crha <mcrha@redhat.com>
2025-01-16 17:25:01 +0100

    utils: Recognize .yml* and .yaml* file extension variants
    The code knows of .yml* and .yaml* variants on various places, this one
    was only missing. This fixes also a bug, when the YAML file was not compressed,
    it was treated as XML.

--- src/as-utils.c
+++ src/as-utils.c
@@ -2578,7 +2578,10 @@ as_utils_install_metadata_file (AsMetadataLocation location,
 
 	switch (as_metadata_file_guess_style (filename)) {
 	case AS_FORMAT_STYLE_CATALOG:
-		if (g_strstr_len (filename, -1, ".yml.gz") != NULL) {
+		if (g_str_has_suffix (filename, ".yml") ||
+		    g_str_has_suffix (filename, ".yml.gz") ||
+		    g_str_has_suffix (filename, ".yaml") ||
+		    g_str_has_suffix (filename, ".yaml.gz")) {
 			path = g_build_filename (as_metadata_location_get_prefix (location),
 						 "swcatalog",
 						 "yaml",
