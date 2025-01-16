57c37df38409bbb876196303ec687f503bc8dac5
Milan Crha <mcrha@redhat.com>
2025-01-16 17:19:10 +0100

    metadata: Add more known extensions into as_metadata_file_guess_style()
    Some parts of the code know of `.yaml*`, but this one knows only about `.yml*`.

--- src/as-metadata.c
+++ src/as-metadata.c
@@ -84,11 +84,17 @@ G_DEFINE_TYPE_WITH_PRIVATE (AsMetadata, as_metadata, G_TYPE_OBJECT)
 AsFormatStyle
 as_metadata_file_guess_style (const gchar *filename)
 {
+	if (g_str_has_suffix (filename, ".xml"))
+		return AS_FORMAT_STYLE_CATALOG;
 	if (g_str_has_suffix (filename, ".xml.gz"))
 		return AS_FORMAT_STYLE_CATALOG;
 	if (g_str_has_suffix (filename, ".yml"))
 		return AS_FORMAT_STYLE_CATALOG;
 	if (g_str_has_suffix (filename, ".yml.gz"))
+		return AS_FORMAT_STYLE_CATALOG;
+	if (g_str_has_suffix (filename, ".yaml"))
+		return AS_FORMAT_STYLE_CATALOG;
+	if (g_str_has_suffix (filename, ".yaml.gz"))
 		return AS_FORMAT_STYLE_CATALOG;
 	if (g_str_has_suffix (filename, ".appdata.xml"))
 		return AS_FORMAT_STYLE_METAINFO;
@@ -100,8 +106,6 @@ as_metadata_file_guess_style (const gchar *filename)
 		return AS_FORMAT_STYLE_METAINFO;
 	if (g_str_has_suffix (filename, ".metainfo.xml.in.in"))
 		return AS_FORMAT_STYLE_METAINFO;
-	if (g_str_has_suffix (filename, ".xml"))
-		return AS_FORMAT_STYLE_CATALOG;
 	return AS_FORMAT_STYLE_UNKNOWN;
 }
 
