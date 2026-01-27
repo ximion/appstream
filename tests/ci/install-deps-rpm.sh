26abd20cf18f7676887830b44e0da4cdc5741f65
Matthias Klumpp <matthias@tenstral.net>
2026-01-27 05:38:26 +0100

    trivial: ci: fedora: Install bash-completion via pkgconfig

--- tests/ci/install-deps-rpm.sh
+++ tests/ci/install-deps-rpm.sh
@@ -15,7 +15,6 @@ dnf --assumeyes --quiet --setopt=install_weak_deps=False install \
     gdb \
     git \
     meson \
-    bash-completion \
     gettext \
     gperf \
     gi-docgen \
@@ -27,6 +26,7 @@ dnf --assumeyes --quiet --setopt=install_weak_deps=False install \
     gcovr \
     xz-devel \
     libuuid-devel \
+    'pkgconfig(bash-completion)' \
     'pkgconfig(gio-2.0)' \
     'pkgconfig(gobject-introspection-1.0)' \
     'pkgconfig(xmlb)' \
