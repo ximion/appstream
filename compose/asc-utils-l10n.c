59a5d9c21426ac5fdecaf058956e653d7cab758a
OrbisAI Security <mediratta01.pally@gmail.com>
2026-05-27 06:26:51 +0530

    fixing the CI errors

--- compose/asc-utils-l10n.c
+++ compose/asc-utils-l10n.c
@@ -120,12 +120,12 @@ asc_l10n_parse_file_gettext (AscLocaleContext *ctx,
 	g_autoptr(GBytes) bytes = NULL;
 	const gchar *data = NULL;
 	gboolean swapped;
+	gsize data_len = 0;
 
 	/* read data */
 	bytes = asc_unit_read_data (unit, filename, error);
 	if (bytes == NULL)
 		return FALSE;
-	gsize data_len = 0;
 	data = g_bytes_get_data (bytes, &data_len);
 
 	/* we only strictly need the header */
@@ -306,10 +306,11 @@ asc_l10n_parse_file_qt (AscLocaleContext *ctx,
 
 	/* parse each section */
 	while (m < len) {
+		guint32 section_len;
 		AscLocaleQmSection section = _read_uint8 (data, &m);
 		if (m + 4 > len)
 			break;
-		guint32 section_len = _read_uint32 (data, &m);
+		section_len = _read_uint32 (data, &m);
 		if (section_len > len - m) {
 			g_set_error_literal (error,
 					     ASC_COMPOSE_ERROR,
