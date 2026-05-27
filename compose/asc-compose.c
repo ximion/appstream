ximion/appstream

commit ba01b6f3c355aad6c719274c1c367d81a4a0f0b8

compose: Implement more flexible icon policy
AppStream Generator has this feature, and it made sense to implement it
for ascompose as well. This also simplifies the integration of compose
and asgen.

compose/asc-compose.c
@@ -62,7 +62,7 @@ typedef struct
 	gchar		*cainfo;
 
 	AscComposeFlags	flags;
-	AscIconPolicy	icon_policy;
+	AscIconPolicy	*icon_policy;
 
 	gchar		*data_result_dir;
 	gchar		*icons_result_dir;
@@ -112,7 +112,9 @@ asc_compose_init (AscCompose *compose)
 			ASC_COMPOSE_FLAG_ALLOW_SCREENCASTS |
 			ASC_COMPOSE_FLAG_PROCESS_FONTS |
 			ASC_COMPOSE_FLAG_PROCESS_TRANSLATIONS;
-	priv->icon_policy = ASC_ICON_POLICY_BALANCED;
+
+	/* the icon policy will initialize with default settings */
+	priv->icon_policy = asc_icon_policy_new ();
 }
 
 static void
@@ -385,28 +387,16 @@ asc_compose_remove_flags (AscCompose *compose, AscComposeFlags flags)
  * asc_compose_get_icon_policy:
  * @compose: an #AscCompose instance.
  *
- * Get the selected icon policy.
+ * Get the policy for how icons should be distributed to
+ * any AppStream clients.
+ *
+ * Returns: (transfer none): an #AscIconPolicy
  */
-AscIconPolicy
+AscIconPolicy*
 asc_compose_get_icon_policy (AscCompose *compose)
 {
 	AscComposePrivate *priv = GET_PRIVATE (compose);
 	return priv->icon_policy;
-}
-
-/**
- * asc_compose_set_icon_policy:
- * @compose: an #AscCompose instance.
- * @policy: an #AscIconPolicy
- *
- * Set a policy for how icons should be distributed to
- * any AppStream clients.
- */
-void
-asc_compose_set_icon_policy (AscCompose *compose, AscIconPolicy policy)
-{
-	AscComposePrivate *priv = GET_PRIVATE (compose);
-	priv->icon_policy = policy;
 }
 
 /**
@@ -932,17 +922,14 @@ asc_compose_process_icons (AscCompose *compose,
 			   AscUnit *unit)
 {
 	AscComposePrivate *priv = GET_PRIVATE (compose);
-	const gint sizes[] = {  48,
-				64,
-				128,
-				-1 };
-	const gint scale_factors[] = {  1,
-					2,
-					-1 };
 	GPtrArray *icons = NULL;
 	g_autoptr(AsIcon) stock_icon = NULL;
 	gboolean stock_icon_found = FALSE;
 	const gchar *icon_name = NULL;
+	AscIconPolicyIter iter;
+	guint size;
+	guint scale_factor;
+	AscIconState icon_state;
 
 	/* do nothing if we have no icons to process */
 	icons = as_component_get_icons (cpt);
@@ -973,145 +960,151 @@ asc_compose_process_icons (AscCompose *compose,
 	}
 
 	icon_name = as_icon_get_name (stock_icon);
-	for (guint k = 0; scale_factors[k] > 0; k++) {
-		for (guint i = 0; sizes[i] > 0; i++) {
-			g_autofree gchar *icon_fname = NULL;
-			g_autofree gchar *res_icon_fname = NULL;
-			g_autofree gchar *res_icon_size_str = NULL;
-			g_autofree gchar *res_icon_sizedir = NULL;
-			g_autofree gchar *res_icon_basename = NULL;
-			g_autoptr(AscImage) img = NULL;
-			g_autoptr(AsIcon) icon = NULL;
-			g_autoptr(GBytes) img_bytes = NULL;
-			gboolean is_vector_icon = FALSE;
-			const void *img_data;
-			gsize img_len;
-			g_autoptr(GError) error = NULL;
-			icon_fname = asc_compose_find_icon_filename (compose,
-									unit,
-									icon_name,
-									sizes[i],
-									scale_factors[k]);
+	asc_icon_policy_iter_init (&iter, priv->icon_policy);
+	while (asc_icon_policy_iter_next (&iter, &size, &scale_factor, &icon_state)) {
+		g_autofree gchar *icon_fname = NULL;
+		g_autofree gchar *res_icon_fname = NULL;
+		g_autofree gchar *res_icon_size_str = NULL;
+		g_autofree gchar *res_icon_sizedir = NULL;
+		g_autofree gchar *res_icon_basename = NULL;
+		g_autoptr(AscImage) img = NULL;
+		g_autoptr(AsIcon) icon = NULL;
+		g_autoptr(GBytes) img_bytes = NULL;
+		gboolean is_vector_icon = FALSE;
+		const void *img_data;
+		gsize img_len;
+		g_autoptr(GError) error = NULL;
 
-			if (icon_fname == NULL) {
-				/* only a 64x64px icon is mandatory, everything else is optional */
-				if (sizes[i] == 64 && scale_factors[k] == 1) {
-					asc_result_add_hint (cres, cpt,
-								"icon-not-found",
-								"icon_fname", icon_name,
-								NULL);
-					return;
-				}
-				continue;
-			}
+		/* skip icon if its size should be skipped */
+		if (icon_state == ASC_ICON_STATE_IGNORE)
+			continue;
 
-			is_vector_icon = g_str_has_suffix (icon_fname, ".svgz") || g_str_has_suffix (icon_fname, ".svg");
-			img_bytes = asc_unit_read_data (unit, icon_fname, &error);
-			if (img_bytes == NULL) {
+		icon_fname = asc_compose_find_icon_filename (compose,
+								unit,
+								icon_name,
+								size,
+								scale_factor);
+
+		if (icon_fname == NULL) {
+			/* only a 64x64px icon is mandatory, everything else is optional */
+			if (size == 64 && scale_factor == 1) {
 				asc_result_add_hint (cres, cpt,
-							"file-read-error",
-							"fname", icon_fname,
-							"msg", error->message,
+							"icon-not-found",
+							"icon_fname", icon_name,
 							NULL);
 				return;
 			}
-			img_data = g_bytes_get_data (img_bytes, &img_len);
-			img = asc_image_new_from_data (img_data, img_len,
-							is_vector_icon? sizes[i] * scale_factors[k] : 0,
-							g_str_has_suffix (icon_fname, ".svgz"),
-							ASC_IMAGE_LOAD_FLAG_ALWAYS_RESIZE,
-							&error);
-			if (img == NULL) {
-				asc_result_add_hint (cres, cpt,
-							"file-read-error",
-							"fname", icon_fname,
-							"msg", error->message,
+			continue;
+		}
+
+		is_vector_icon = g_str_has_suffix (icon_fname, ".svgz") || g_str_has_suffix (icon_fname, ".svg");
+		img_bytes = asc_unit_read_data (unit, icon_fname, &error);
+		if (img_bytes == NULL) {
+			asc_result_add_hint (cres, cpt,
+						"file-read-error",
+						"fname", icon_fname,
+						"msg", error->message,
+						NULL);
+			return;
+		}
+		img_data = g_bytes_get_data (img_bytes, &img_len);
+		img = asc_image_new_from_data (img_data, img_len,
+						is_vector_icon? size * scale_factor : 0,
+						g_str_has_suffix (icon_fname, ".svgz"),
+						ASC_IMAGE_LOAD_FLAG_ALWAYS_RESIZE,
+						&error);
+		if (img == NULL) {
+			asc_result_add_hint (cres, cpt,
+						"file-read-error",
+						"fname", icon_fname,
+						"msg", error->message,
+						NULL);
+			return;
+		}
+
+		/* we only take exact-ish size matches for 48x48px */
+		if (size == 48 && asc_image_get_width (img) > 48)
+			continue;
+
+		res_icon_size_str = (scale_factor == 1)?
+					g_strdup_printf ("%ix%i",
+							 size, size)
+					: g_strdup_printf ("%ix%i@%i",
+							   size, size,
+							   scale_factor);
+		res_icon_sizedir = g_build_filename (priv->icons_result_dir, res_icon_size_str, NULL);
+
+		g_mkdir_with_parents (res_icon_sizedir, 0755);
+		res_icon_basename = g_strdup_printf ("%s.png", as_component_get_id (cpt));
+		res_icon_fname = g_build_filename (res_icon_sizedir,
+							res_icon_basename,
 							NULL);
-				return;
-			}
 
-			/* we only take exact-ish size matches for 48x48px */
-			if (sizes[i] == 48 && asc_image_get_width (img) > 48)
-				continue;
+		/* scale & save the image */
+		g_debug ("Saving icon: %s", res_icon_fname);
+		if (!asc_image_save_filename (img,
+						res_icon_fname,
+						size * scale_factor, size * scale_factor,
+						ASC_IMAGE_SAVE_FLAG_OPTIMIZE,
+						&error)) {
+			asc_result_add_hint (cres, cpt,
+						"icon-write-error",
+						"fname", icon_fname,
+						"msg", error->message,
+						NULL);
+			return;
+		}
 
-			res_icon_size_str = (scale_factors[k] == 1)?
-						g_strdup_printf ("%ix%i",
-								 sizes[i], sizes[i])
-						: g_strdup_printf ("%ix%i@%i",
-								   sizes[i], sizes[i],
-								   scale_factors[k]);
-			res_icon_sizedir = g_build_filename (priv->icons_result_dir, res_icon_size_str, NULL);
-
-			g_mkdir_with_parents (res_icon_sizedir, 0755);
-			res_icon_basename = g_strdup_printf ("%s.png", as_component_get_id (cpt));
-			res_icon_fname = g_build_filename (res_icon_sizedir,
-							   res_icon_basename,
-							   NULL);
-
-			/* scale & save the image */
-			g_debug ("Saving icon: %s", res_icon_fname);
-			if (!asc_image_save_filename (img,
-							res_icon_fname,
-							sizes[i] * scale_factors[k], sizes[i] * scale_factors[k],
-							ASC_IMAGE_SAVE_FLAG_OPTIMIZE,
-							&error)) {
-				asc_result_add_hint (cres, cpt,
-							"icon-write-error",
-							"fname", icon_fname,
-							"msg", error->message,
-							NULL);
-				return;
-			}
-
-			/* create a remote reference if we have data for it */
-			if (priv->media_result_dir != NULL) {
-				g_autofree gchar *icons_media_urlpart_dir = NULL;
-				g_autofree gchar *icons_media_urlpart_fname = NULL;
-				g_autofree gchar *icons_media_path = NULL;
-				g_autofree gchar *icon_media_fname = NULL;
-				g_autoptr(AsIcon) remote_icon = NULL;
-				icons_media_urlpart_dir = g_strdup_printf ("%s/%s/%s",
-								           asc_result_gcid_for_component (cres, cpt),
-								           "icons",
-								           res_icon_size_str);
-				icons_media_urlpart_fname = g_strdup_printf ("%s/%s",
-									     icons_media_urlpart_dir,
-									     res_icon_basename);
-				icons_media_path = g_build_filename (priv->media_result_dir,
+		/* create a remote reference if we have data for it */
+		if (priv->media_result_dir != NULL && icon_state != ASC_ICON_STATE_CACHED_ONLY) {
+			g_autofree gchar *icons_media_urlpart_dir = NULL;
+			g_autofree gchar *icons_media_urlpart_fname = NULL;
+			g_autofree gchar *icons_media_path = NULL;
+			g_autofree gchar *icon_media_fname = NULL;
+			g_autoptr(AsIcon) remote_icon = NULL;
+			icons_media_urlpart_dir = g_strdup_printf ("%s/%s/%s",
+								   asc_result_gcid_for_component (cres, cpt),
+								   "icons",
+								   res_icon_size_str);
+			icons_media_urlpart_fname = g_strdup_printf ("%s/%s",
 								     icons_media_urlpart_dir,
-								     NULL);
-				icon_media_fname = g_build_filename (icons_media_path,
-								     res_icon_basename,
-								     NULL);
-				g_mkdir_with_parents (icons_media_path, 0755);
+								     res_icon_basename);
+			icons_media_path = g_build_filename (priv->media_result_dir,
+								icons_media_urlpart_dir,
+								NULL);
+			icon_media_fname = g_build_filename (icons_media_path,
+								res_icon_basename,
+								NULL);
+			g_mkdir_with_parents (icons_media_path, 0755);
 
-				g_debug ("Adding media pool icon: %s", icon_media_fname);
-				if (!as_copy_file (res_icon_fname, icon_media_fname, &error)) {
-					g_warning ("Unable to write media pool icon: %s", icon_media_fname);
-					asc_result_add_hint (cres, cpt,
-							     "icon-write-error",
-							     "fname", icon_fname,
-							     "msg", error->message,
-							     NULL);
-					return;
-				}
-
-				/* add remote icon to metadata */
-				remote_icon = as_icon_new ();
-				as_icon_set_kind (remote_icon, AS_ICON_KIND_REMOTE);
-				as_icon_set_width (remote_icon, sizes[i]);
-				as_icon_set_height (remote_icon, sizes[i]);
-				as_icon_set_scale (remote_icon, scale_factors[k]);
-				as_icon_set_url (remote_icon, icons_media_urlpart_fname);
-				as_component_add_icon (cpt, remote_icon);
+			g_debug ("Adding media pool icon: %s", icon_media_fname);
+			if (!as_copy_file (res_icon_fname, icon_media_fname, &error)) {
+				g_warning ("Unable to write media pool icon: %s", icon_media_fname);
+				asc_result_add_hint (cres, cpt,
+						     "icon-write-error",
+						     "fname", icon_fname,
+						     "msg", error->message,
+						     NULL);
+				return;
 			}
 
-			/* add icon to metadata */
+			/* add remote icon to metadata */
+			remote_icon = as_icon_new ();
+			as_icon_set_kind (remote_icon, AS_ICON_KIND_REMOTE);
+			as_icon_set_width (remote_icon, size);
+			as_icon_set_height (remote_icon, size);
+			as_icon_set_scale (remote_icon, scale_factor);
+			as_icon_set_url (remote_icon, icons_media_urlpart_fname);
+			as_component_add_icon (cpt, remote_icon);
+		}
+
+		/* add icon to metadata */
+		if (icon_state != ASC_ICON_STATE_REMOTE_ONLY) {
 			icon = as_icon_new ();
 			as_icon_set_kind (icon, AS_ICON_KIND_CACHED);
-			as_icon_set_width (icon, sizes[i]);
-			as_icon_set_height (icon, sizes[i]);
-			as_icon_set_scale (icon, scale_factors[k]);
+			as_icon_set_width (icon, size);
+			as_icon_set_height (icon, size);
+			as_icon_set_scale (icon, scale_factor);
 			as_icon_set_name (icon, res_icon_basename);
 			as_component_add_icon (cpt, icon);
 		}

compose/appstream-compose.h
@@ -31,6 +31,7 @@
 
 #include <asc-globals.h>
 #include <asc-compose.h>
+#include <asc-icon-policy.h>
 #include <asc-utils.h>
 #include <asc-hint.h>
 #include <asc-unit.h>

compose/asc-compose.h
@@ -28,6 +28,7 @@
 
 #include "asc-unit.h"
 #include "asc-result.h"
+#include "asc-icon-policy.h"
 
 G_BEGIN_DECLS
 
@@ -77,22 +78,6 @@ typedef enum {
 	ASC_COMPOSE_FLAG_PROPAGATE_ARTIFACTS	 	= 1 << 10,
 	ASC_COMPOSE_FLAG_NO_FINAL_CHECK		 	= 1 << 11,
 } AscComposeFlags;
-
-/**
- * AscIconPolicy:
- * @ASC_ICON_POLICY_BALANCED:		Create cache for all icons, but provide "remote" links for the bigger icons.
- * @ASC_ICON_POLICY_CACHE_REMOTE:	Create cache for all icons, and provide "remote" links for all.
- * @ASC_ICON_POLICY_ONLY_CACHED:	Do not create remote icons, only cached ones are possible.
- * @ASC_ICON_POLICY_ONLY_REMOTE:	Create only remote icons, do not create cached ones.
- *
- * Policy for how to deliver icons.
- **/
-typedef enum {
-	ASC_ICON_POLICY_BALANCED,
-	ASC_ICON_POLICY_CACHE_REMOTE,
-	ASC_ICON_POLICY_ONLY_CACHED,
-	ASC_ICON_POLICY_ONLY_REMOTE
-} AscIconPolicy;
 
 /**
  * AscCheckMetadataEarlyFn:
@@ -167,9 +152,7 @@ void			asc_compose_add_flags (AscCompose *compose,
 void			asc_compose_remove_flags (AscCompose *compose,
 						  AscComposeFlags flags);
 
-AscIconPolicy		asc_compose_get_icon_policy (AscCompose *compose);
-void			asc_compose_set_icon_policy (AscCompose *compose,
-							AscIconPolicy policy);
+AscIconPolicy		*asc_compose_get_icon_policy (AscCompose *compose);
 
 const gchar		*asc_compose_get_cainfo (AscCompose *compose);
 void			asc_compose_set_cainfo (AscCompose *compose,

compose/asc-icon-policy.c
@@ -1,0 +1,248 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2016-2022 Matthias Klumpp <matthias@tenstral.net>
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * SECTION:asc-icon-policy
+ * @short_description: Set policy on how to deal with different icon types.
+ * @include: appstream-compose.h
+ */
+
+#include "config.h"
+#include "asc-icon-policy.h"
+
+#include "as-utils-private.h"
+#include "asc-globals-private.h"
+
+typedef struct
+{
+	GPtrArray		*entries;
+} AscIconPolicyPrivate;
+
+G_DEFINE_TYPE_WITH_PRIVATE (AscIconPolicy, asc_icon_policy, G_TYPE_OBJECT)
+#define GET_PRIVATE(o) (asc_icon_policy_get_instance_private (o))
+
+/**
+ * AscIconPolicyIter:
+ *
+ * A #AscIconPolicyIter structure represents an iterator that can be used
+ * to iterate over the icon sizes / policy entries of an #AscIconPolicy.
+ * #AscIconPolicyIter structures are typically allocated on the stack and
+ * then initialized with asc_icon_policy_iter_init().
+ */
+
+typedef struct
+{
+	AscIconPolicy 	*ipolicy;
+	guint		pos;
+	gpointer	dummy3;
+	gpointer	dummy4;
+	gpointer	dummy5;
+	gpointer	dummy6;
+} RealIconPolicyIter;
+
+G_STATIC_ASSERT (sizeof (AscIconPolicyIter) == sizeof (RealIconPolicyIter));
+
+typedef struct {
+	guint		size;
+	guint		scale;
+	AscIconState	state;
+} AscIconPolicyEntry;
+
+static AscIconPolicyEntry*
+asc_icon_policy_entry_new (guint size, guint scale)
+{
+	AscIconPolicyEntry *entry;
+	entry = g_slice_new0 (AscIconPolicyEntry);
+	entry->size = size;
+	entry->scale = scale;
+	entry->state = ASC_ICON_STATE_IGNORE;
+	return entry;
+}
+
+static void
+asc_icon_policy_entry_free (AscIconPolicyEntry *entry)
+{
+	g_slice_free (AscIconPolicyEntry, entry);
+}
+
+static void
+asc_icon_policy_init (AscIconPolicy *ipolicy)
+{
+	AscIconPolicyPrivate *priv = GET_PRIVATE (ipolicy);
+	priv->entries = g_ptr_array_new_with_free_func ((GDestroyNotify) asc_icon_policy_entry_free);
+
+	/* set our default policy */
+	asc_icon_policy_set_policy (ipolicy, 48, 1, ASC_ICON_STATE_CACHED_ONLY);
+	asc_icon_policy_set_policy (ipolicy, 48, 2, ASC_ICON_STATE_CACHED_ONLY);
+	asc_icon_policy_set_policy (ipolicy, 64, 1, ASC_ICON_STATE_CACHED_ONLY);
+	asc_icon_policy_set_policy (ipolicy, 64, 2, ASC_ICON_STATE_CACHED_ONLY);
+	asc_icon_policy_set_policy (ipolicy, 128, 1, ASC_ICON_STATE_CACHED_REMOTE);
+	asc_icon_policy_set_policy (ipolicy, 128, 2, ASC_ICON_STATE_CACHED_REMOTE);
+}
+
+static void
+asc_icon_policy_finalize (GObject *object)
+{
+	AscIconPolicy *ipolicy = ASC_ICON_POLICY (object);
+	AscIconPolicyPrivate *priv = GET_PRIVATE (ipolicy);
+
+	g_ptr_array_unref (priv->entries);
+
+	G_OBJECT_CLASS (asc_icon_policy_parent_class)->finalize (object);
+}
+
+static void
+asc_icon_policy_class_init (AscIconPolicyClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+	object_class->finalize = asc_icon_policy_finalize;
+}
+
+/**
+ * asc_icon_policy_set_policy:
+ * @ipolicy: an #AscIconPolicy instance.
+ * @icon_size: the size of the icon to set policy for (e.g. 64 for 64x64px icons)
+ * @icon_scale: the icon scale factor, e.g. 1
+ * @state: the designated #AscIconState
+ *
+ * Sets a designated state for an icon of the given size.
+ */
+void
+asc_icon_policy_set_policy (AscIconPolicy *ipolicy,
+			    guint icon_size,
+			    guint icon_scale,
+			    AscIconState state)
+{
+	AscIconPolicyPrivate *priv = GET_PRIVATE (ipolicy);
+	AscIconPolicyEntry *entry = NULL;
+
+	if (icon_scale < 1) {
+		g_warning ("An icon scale of 0 is invalid, resetting to 1.");
+		icon_scale = 1;
+	}
+
+	for (guint i = 0; i < priv->entries->len; i++) {
+		AscIconPolicyEntry *e = g_ptr_array_index (priv->entries, i);
+		if (e->size == icon_size && e->scale == icon_scale) {
+			entry = e;
+			break;
+		}
+	}
+
+	if (entry != NULL) {
+		/* found an existing entry, just modify that */
+		entry->state = state;
+		return;
+	}
+
+	entry = asc_icon_policy_entry_new (icon_size, icon_scale);
+	entry->state = state;
+	g_ptr_array_add (priv->entries, entry);
+}
+
+/**
+ * asc_icon_policy_iter_init:
+ * @iter: an uninitialized #AscIconPolicyIter
+ * @ipolicy: an #AscIconPolicy
+ *
+ * Initializes a policy iterator for the policy entry list and associates it
+ * it with @ipolicy.
+ * The #AscIconPolicyIter structure is typically allocated on the stack
+ * and does not need to be freed explicitly.
+ */
+void
+asc_icon_policy_iter_init (AscIconPolicyIter *iter, AscIconPolicy *ipolicy)
+{
+	RealIconPolicyIter *ri = (RealIconPolicyIter *) iter;
+
+	g_return_if_fail (iter != NULL);
+	g_return_if_fail (ASC_IS_ICON_POLICY (ipolicy));
+
+	ri->ipolicy = ipolicy;
+	ri->pos = 0;
+}
+
+/**
+ * asc_icon_policy_iter_next:
+ * @iter: an initialized #AscIconPolicyIter
+ * @size: (out) (optional) (not nullable): Destination of the returned icon size
+ * @scale: (out) (optional) (not nullable): Destination of the returned icon scale factor
+ * @state: (out) (optional) (nullable): Destination of the returned designated icon state.
+ *
+ * Returns the current icon policy entry and advances the iterator.
+ * Example:
+ * |[<!-- language="C" -->
+ * AscIconPolicyIter iter;
+ * guint icon_size;
+ * guint icon_scale;
+ * AscIconState istate;
+ *
+ * xb_node_child_iter_init (&iter, ipolicy);
+ * while (xb_node_child_iter_next (&iter, &icon_size, &icon_scale, &istate)) {
+ *     // do something with the icon entry data
+ * }
+ * ]|
+ *
+ * Returns: %FALSE if the last entry has been reached.
+ */
+gboolean
+asc_icon_policy_iter_next (AscIconPolicyIter *iter,
+			   guint *size,
+			   guint *scale,
+			   AscIconState *state)
+{
+	AscIconPolicyPrivate *priv;
+	AscIconPolicyEntry *entry = NULL;
+	RealIconPolicyIter *ri = (RealIconPolicyIter *) iter;
+
+	g_return_val_if_fail (iter != NULL, FALSE);
+	g_return_val_if_fail (size != NULL, FALSE);
+	g_return_val_if_fail (scale != NULL, FALSE);
+	priv = GET_PRIVATE (ri->ipolicy);
+
+	/* check if the iteration was finished */
+	if (ri->pos >= priv->entries->len) {
+		*size = 0;
+		*scale = 0;
+		return FALSE;
+	}
+
+	entry = g_ptr_array_index (priv->entries, ri->pos);
+	ri->pos++;
+	*size = entry->size;
+	*scale = entry->scale;
+	if (state != NULL)
+		*state = entry->state;
+
+	return TRUE;
+}
+
+/**
+ * asc_icon_policy_new:
+ *
+ * Creates a new #AscIconPolicy.
+ **/
+AscIconPolicy*
+asc_icon_policy_new (void)
+{
+	AscIconPolicy *ipolicy;
+	ipolicy = g_object_new (ASC_TYPE_ICON_POLICY, NULL);
+	return ASC_ICON_POLICY (ipolicy);
+}

compose/asc-icon-policy.h
@@ -1,0 +1,85 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2016-2022 Matthias Klumpp <matthias@tenstral.net>
+ *
+ * Licensed under the GNU Lesser General Public License Version 2.1
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 2.1 of the license, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if !defined (__APPSTREAM_COMPOSE_H) && !defined (ASC_COMPILATION)
+#error "Only <appstream-compose.h> can be included directly."
+#endif
+#pragma once
+
+#include <glib-object.h>
+#include <appstream.h>
+
+G_BEGIN_DECLS
+
+#define ASC_TYPE_ICON_POLICY (asc_icon_policy_get_type ())
+G_DECLARE_DERIVABLE_TYPE (AscIconPolicy, asc_icon_policy, ASC, ICON_POLICY, GObject)
+
+struct _AscIconPolicyClass
+{
+	GObjectClass parent_class;
+	/*< private >*/
+	void (*_as_reserved1) (void);
+	void (*_as_reserved2) (void);
+	void (*_as_reserved3) (void);
+	void (*_as_reserved4) (void);
+};
+
+typedef struct
+{
+  /*< private >*/
+  gpointer	dummy1;
+  guint		dummy2;
+  gpointer	dummy3;
+  gpointer	dummy4;
+  gpointer	dummy5;
+  gpointer	dummy6;
+} AscIconPolicyIter;
+
+/**
+ * AscIconState:
+ * @ASC_ICON_STATE_IGNORE:		Ignore icons of this size.
+ * @ASC_ICON_STATE_CACHED_REMOTE:	Create cache for the icon, and provide "remote" link as well.
+ * @ASC_ICON_STATE_CACHED_ONLY:		Only create cache for this icon.
+ * @ASC_ICON_STATE_REMOTE_ONLY:		Only create remote link for this icon.
+ *
+ * Policy for how to store icon of a given size.
+ **/
+typedef enum {
+	ASC_ICON_STATE_IGNORE,
+	ASC_ICON_STATE_CACHED_REMOTE,
+	ASC_ICON_STATE_CACHED_ONLY,
+	ASC_ICON_STATE_REMOTE_ONLY
+} AscIconState;
+
+AscIconPolicy			*asc_icon_policy_new (void);
+
+void				asc_icon_policy_set_policy (AscIconPolicy *ipolicy,
+							    guint icon_size,
+							    guint icon_scale,
+							    AscIconState state);
+
+void				asc_icon_policy_iter_init (AscIconPolicyIter *iter,
+							   AscIconPolicy *ipolicy);
+gboolean			asc_icon_policy_iter_next (AscIconPolicyIter *iter,
+							   guint *size,
+							   guint *scale,
+							   AscIconState *state);
+
+G_END_DECLS

commit 84cdb036c8c7632d07ab7ef89471f4644381b7b6

compose: Unref icon_policy in asc_compose_finalize
```
Indirect leak of 12 byte(s) in 1 object(s) allocated from:
    #0 0x557432e34af8 in malloc (/__w/flatpak-builder/flatpak-builder/builddir/src/flatpak-builder+0x16baf8) (BuildId: fedaf9746ee75c7de57cf0bb3c5eb79d9c0f8dd8)
    #1 0x7f95fd995679 in g_malloc (/usr/lib/x86_64-linux-gnu/libglib-2.0.so.0+0x67679) (BuildId: 2426ee6f828a10bf4da963a4c474752c33742359)
    #2 0x7f95fd3492c4 in asc_icon_policy_set_policy (/usr/lib/x86_64-linux-gnu/libappstream-compose.so.0+0x232c4) (BuildId: 71a33e272677c786f5751c25b4fda845d03010e2)
    #3 0x7f95fd72fac0 in g_type_create_instance (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x42ac0) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #4 0x7f95fd7153e3  (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x283e3) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #5 0x7f95fd7169e6 in g_object_new_with_properties (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x299e6) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #6 0x7f95fd7179c0 in g_object_new (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x2a9c0) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #7 0x7f95fd340ce7  (/usr/lib/x86_64-linux-gnu/libappstream-compose.so.0+0x1ace7) (BuildId: 71a33e272677c786f5751c25b4fda845d03010e2)
    #8 0x7f95fd72fac0 in g_type_create_instance (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x42ac0) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #9 0x7f95fd7153e3  (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x283e3) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #10 0x7f95fd7169e6 in g_object_new_with_properties (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x299e6) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #11 0x7f95fd7179c0 in g_object_new (/usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0+0x2a9c0) (BuildId: 0f9dc57097a22c42f8b9d339c75528f2205b3639)
    #12 0x557432ed3611 in builder_appstreamcli_compose /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-manifest.c:2467:13
    #13 0x557432ecf859 in builder_manifest_cleanup /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-manifest.c:3138:16
    #14 0x557432ebb659 in main /__w/flatpak-builder/flatpak-builder/builddir/../src/builder-main.c:1027:12
    #15 0x7f95fcfa4600  (/usr/lib/x86_64-linux-gnu/libc.so.6+0x2a600) (BuildId: 90ebd03ae9d9f42b23b4eb82fdf70352cf744198)
    #16 0x7f95fcfa4717 in __libc_start_main (/usr/lib/x86_64-linux-gnu/libc.so.6+0x2a717) (BuildId: 90ebd03ae9d9f42b23b4eb82fdf70352cf744198)
    #17 0x557432d8f554 in _start (/__w/flatpak-builder/flatpak-builder/builddir/src/flatpak-builder+0xc6554) (BuildId: fedaf9746ee75c7de57cf0bb3c5eb79d9c0f8dd8)
```

Fixes: ba01b6f3

compose/asc-compose.c
@@ -132,6 +132,8 @@ asc_compose_finalize (GObject *object)
 	if (priv->locale_unit != NULL)
 		g_object_unref (priv->locale_unit);
 
+	g_object_unref (priv->icon_policy);
+
 	g_mutex_clear (&priv->mutex);
 
 	G_OBJECT_CLASS (asc_compose_parent_class)->finalize (object);