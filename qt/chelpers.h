a350713cfa7e24a3ad94b8104983eef3320f501c
Matthias Klumpp <matthias@tenstral.net>
2026-07-24 04:17:43 +0200

    qt: Use strndup for C string-list conversion
    This is a bit nicer to read.

--- qt/chelpers.h
+++ qt/chelpers.h
@@ -80,11 +80,10 @@ inline QStringList valueWrap(GList *list)
 
 inline char **stringListToCharArray(const QStringList &list)
 {
-    char **array = (char **) g_malloc(sizeof(char *) * (list.size() + 1));
+    char **array = g_new(char *, list.size() + 1);
     for (int i = 0; i < list.size(); ++i) {
         const QByteArray string = list[i].toUtf8();
-        array[i] = (char *) g_malloc(sizeof(char) * (string.size() + 1));
-        g_strlcpy(array[i], string.constData(), string.size() + 1);
+        array[i] = g_strndup(string.constData(), string.size());
     }
     array[list.size()] = nullptr;
     return array;
